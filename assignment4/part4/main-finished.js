8dbeb3bb4966f5c7d48dca0fd9dd25f319e36be0
subh-28 <brarsubh882@gmail.com>
2025-08-01 19:28:22 -0400

    checkbounds

--- assignment4/part4/main-finished.js
+++ assignment4/part4/main-finished.js
@@ -114,7 +114,20 @@ draw(){
     ctx.arc(this.x, this.y, this.size, 0, 2* Math.PI);
     ctx.stroke();
 }
-
+checkBounds(){
+    if((this.x + this.size) <= width){
+        this.x = width - this.size;
+    }
+    if ((this.x - this.size) <= 0){
+        this.x = this.size;
+    }
+    if((this.y + this.size) >= height){
+        this.y = height - this.size;
+    }
+    if ((this.y - this.size) <= 0){
+        this.y = this.size;
+    }
+}
 }
 
 const balls = [];
