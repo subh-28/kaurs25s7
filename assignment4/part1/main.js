d50b902620f3e2ea15b9060b5cef2e139a693d34
subh-28 <brarsubh882@gmail.com>
2025-08-01 15:10:48 -0400

    defined the variables in the javascript

---
+++ assignment4/part1/main.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+    return array[Math.floor(Math.random()* array.length)];
+
+}
