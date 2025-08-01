23ae003e5c6d9b48874eb7586f482c4834335d6a
subh-28 <brarsubh882@gmail.com>
2025-08-01 19:24:50 -0400

    draw the evil circle

--- assignment4/part4/main-finished.js
+++ assignment4/part4/main-finished.js
@@ -107,9 +107,15 @@ class EvilCircle extends Shape{
   }
 }); 
     }
-
+draw(){
+    ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = 3;
+    ctx.arc(this.x, this.y, this.size, 0, 2* Math.PI);
+    ctx.stroke();
 }
 
+}
 
 const balls = [];
 
