59a1ca7ecdd646432e49024e2debcc0b1db58f24
subh-28 <brarsubh882@gmail.com>
2025-08-01 18:50:25 -0400

    added stuff to animating balls

--- assignment4/part3/main.js
+++ assignment4/part3/main.js
@@ -95,6 +95,17 @@ while (balls.length < 25) {
   balls.push(ball);
 }
 
+function loop() {
+  ctx.fillStyle = "rgb(0 0 0 / 25%)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+  }
+
+  requestAnimationFrame(loop);
+}
 
 
 
