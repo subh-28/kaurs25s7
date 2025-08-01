04da25c10fe2aec2db984fd0963d870898e9ae8f
subh-28 <brarsubh882@gmail.com>
2025-08-01 19:18:30 -0400

    updated the collision part

--- assignment4/part4/main-finished.js
+++ assignment4/part4/main-finished.js
@@ -72,7 +72,7 @@ class Ball {
 
   collisionDetect() {
     for (const ball of balls) {
-      if (!(this === ball)) {
+      if (!(this === ball) && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
@@ -83,7 +83,9 @@ class Ball {
       }
     }
   }
+  // …
 }
+
 
 const balls = [];
 
