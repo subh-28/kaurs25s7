edd212b88efe4bb5bdcc6e338cd2e6a95b3809b1
subh-28 <brarsubh882@gmail.com>
2025-08-01 18:41:18 -0400

    adding the text files and add the intial comments

---
+++ assignment4/part3/main.js
@@ -0,0 +1,24 @@
+// Name: subhpreet KAur
+// File: main.js in part 2 of assignment 4
+// Date: 1 August 2025
+// Description: he bouncing balls animation
+
+// setup canvas
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+// function to generate random number
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// function to generate random color
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
